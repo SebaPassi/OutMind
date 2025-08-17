48aa38d817c5b12f45ae24a111216a1f949c9af9
Sebastián Passi <sebastian.passim@uc.cl>
2025-08-17 17:12:21 -0400

    initial changes

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
+import Logo from '../assets/OutMind_Logo.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Buena! HOME</Text>
+    <View style={styles.container}>
+      <Image source={Logo}></Image>
+      <Text style={styles.title}>OutMind</Text>
+      <Text style={{marginBottom: 30, marginTop: 10}}>Tu app de recordatorios favorita</Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  img: {
+    marginVertical: 20
+  }
+})
